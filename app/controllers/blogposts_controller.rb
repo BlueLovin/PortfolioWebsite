37e0dbece2cdcdf39e9809273b70a4f464af952f
Matthew Jury <matthewjury2@gmail.com>
2021-10-17 00:43:58 -0400

    change env variable in blogpost controller

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,6 +1,6 @@
 class BlogpostsController < ApplicationController
     before_action :set_blogpost, only: [:show, :edit, :update, :destroy]
-    http_basic_authenticate_with :name => ENV["USER"], :password => ENV["PASSWORD"], except: :show
+    http_basic_authenticate_with :name => ENV["ADMIN_USERNAME"], :password => ENV["PASSWORD"], except: :show
   
   
     # GET /blogposts
